dd2732acd443ed98952675cf885f9970b0485a73
Donnie Adams <donald.g.adams@me.com>
2016-09-09 13:20:06 -0700

    Finished challenge 10

--- Set2.go
+++ Set2.go
@@ -1,6 +1,10 @@
 package crypto
 
-import "bytes"
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+)
 
 func PadToNBytes(text []byte, N int) []byte {
 	BytesToAdd := N - len(text)
@@ -8,4 +12,34 @@ func PadToNBytes(text []byte, N int) []byte {
 		text = append(text, bytes.Repeat([]byte{byte(BytesToAdd)}, BytesToAdd)...)
 	}
 	return text
+}
+
+func DecryptAESCBC(CipherText, key []byte) []byte {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	if len(CipherText) < aes.BlockSize {
+		panic("ciphertext too short")
+	}
+	iv := make([]byte, aes.BlockSize)
+	for i := 0; i < aes.BlockSize; i++ {
+		iv[i] = 0
+	}
+
+	// CBC mode always works in whole blocks.
+	if len(CipherText)%aes.BlockSize != 0 {
+		panic("ciphertext is not a multiple of the block size")
+	}
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+
+	for i := 0; i < len(CipherText)/aes.BlockSize; i++ {
+		// CryptBlocks can work in-place if the two arguments are the same.
+		mode.CryptBlocks(CipherText[i*aes.BlockSize:(i+1)*aes.BlockSize], CipherText[i*aes.BlockSize:(i+1)*aes.BlockSize])
+	}
+	return CipherText
 }
--- Set2_test.go
+++ Set2_test.go
@@ -1,8 +1,22 @@
 package crypto
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+)
 
 func ExamplePadToNBytes() {
 	fmt.Println(string(PadToNBytes([]byte("YELLOW SUBMARINE"), 20)))
 	// Output: YELLOW SUBMARINE
+}
+
+func ExampleDecryptAESCBC() {
+	FileText, _ := ioutil.ReadFile("Set2_10.txt")
+	ExpectedOutput, _ := ioutil.ReadFile("Set2_10Output.txt")
+	FileTextAsBytes, _ := base64.StdEncoding.DecodeString(string(FileText))
+	PlainText := DecryptAESCBC(FileTextAsBytes, []byte("YELLOW SUBMARINE"))
+	fmt.Println(bytes.Compare(PlainText, ExpectedOutput))
+	// Output: 0
 }
